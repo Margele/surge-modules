2149d9c5e569da741075468a685629bd08ae4976
Ichinomiya Shirona <shirona@ichinomiya.dev>
2025-03-05 21:04:04 +0800

    Update bilibili-share-hook.js

--- bilibili-share-hook.js
+++ bilibili-share-hook.js
@@ -95,4 +95,6 @@ if ($request.url.includes("share/click")) {
     processShare();
 } else if ($request.url.includes("/share/channels") && $request.url.includes("share_origin=vinfo_player")) {
     processChannels();
+} else {
+    $done({});
 }
